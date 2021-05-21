3462e7a95356ad2d47f3fe64ba2f671d12ea8451
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-20 20:39:26 -0500

    Added CSS and JS to confirm relationships set up correctly

---
+++ frontend/src/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded",function(){
+    let p = document.createElement('p');
+    p.innerText = "This is only a test";
+    document.getElementById("main-msg").appendChild(p);
+});
