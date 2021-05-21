09e5a5432802930b94a0ad5123e1a563c9d402dd
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-20 22:50:35 -0500

    Basic dropdown select for organization with event listener for change of select option

--- frontend/src/organization.js
+++ frontend/src/organization.js
@@ -1,5 +1,15 @@
 class Organization {
     static createOrgList(orgs) {
         console.log(orgs);
+        const select = document.querySelector('#org-names');
+
+        for (const org of orgs) {
+            const option = document.createElement("option");
+            option.text = org.name;
+            select.add(option);
+        }
+        select.addEventListener('change', event => {
+            console.log("Changed");
+        })
     }
 }
