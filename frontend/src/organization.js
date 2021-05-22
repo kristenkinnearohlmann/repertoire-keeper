cab700eed464b598230c2e380c04dd034d106dbf
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-21 20:40:23 -0500

    Worked on getting the value back from the select list

--- frontend/src/organization.js
+++ frontend/src/organization.js
@@ -1,4 +1,10 @@
 class Organization {
+    constructor(id, name, org_type) {
+        this._id = id;
+        this._name = name;
+        this._org_type = org_type;
+    }
+
     static createOrgList(orgs) {
         console.log(orgs);
         const select = document.querySelector('#org-names');
@@ -6,10 +12,14 @@ class Organization {
         for (const org of orgs) {
             const option = document.createElement("option");
             option.text = org.name;
+            option.value = org.id;
             select.add(option);
         }
         select.addEventListener('change', event => {
             console.log("Changed");
+            const selected = event.target;
+            console.log(selected.value);
+            console.log(selected[selected.value].text);
         })
     }
 }
