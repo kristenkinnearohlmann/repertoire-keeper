83fa306d26738327e1d557b190f9180f68c4a3c5
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-22 12:56:37 -0500

    Restructing JS for better organization of organization

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,46 @@
-const BASE_URL = "http://localhost:3000/";
-const ORGS_URL = `${BASE_URL}organizations`;
+const BASE_URL = "http://localhost:3000";
+const orgNames = document.getElementById("org-names");
 
-document.addEventListener("DOMContentLoaded",function(){
-    let p = document.createElement('p');
-    p.innerText = "Information for organization's performances will appear here.";
-    document.getElementById("main-msg").appendChild(p);
-    listOrgs();
-});
+// structure page for use
+const init = () => {
+    getOrgNames();
+};
 
-function listOrgs() {
-    return fetch(ORGS_URL)
+
+const getOrgNames = () => {
+    fetch(`${BASE_URL}/organizations`)
         .then(response => {
             return response.json();
         })
-        .then(orgs => {
-            Organization.createOrgList(orgs);
-        });
+        .then(data => {
+            for (const org of data) {
+                const { name, id } = org;
+                const option = document.createElement("option");
+                option.text = name;
+                option.value = id;
+                orgNames.add(option);
+            }
+        })
 };
+
+// load page
+init();
+
+// const ORGS_URL = `${BASE_URL}organizations`;
+
+// document.addEventListener("DOMContentLoaded",function(){
+//     let p = document.createElement('p');
+//     p.innerText = "Information for organization's performances will appear here.";
+//     document.getElementById("main-msg").appendChild(p);
+//     listOrgs();
+// });
+
+// function listOrgs() {
+//     return fetch(ORGS_URL)
+//         .then(response => {
+//             return response.json();
+//         })
+//         .then(orgs => {
+//             Organization.createOrgList(orgs);
+//         });
+// };
--- frontend/src/organization.js
+++ frontend/src/organization.js
@@ -1,60 +1,60 @@
-class Organization {
-    constructor(id, name, org_type, year_founded, url, org_descr) {
-        this._id = id;
-        this._name = name;
-        this._org_type = org_type;
-        this._year_founded = year_founded;
-        this._url = url;
-        this._org_descr = org_descr;
-    }
+// class Organization {
+//     constructor(id, name, org_type, year_founded, url, org_descr) {
+//         this._id = id;
+//         this._name = name;
+//         this._org_type = org_type;
+//         this._year_founded = year_founded;
+//         this._url = url;
+//         this._org_descr = org_descr;
+//     }
 
-    static createOrgList(orgs) {
-        const select = document.querySelector('#org-names');
+//     static createOrgList(orgs) {
+//         const select = document.querySelector('#org-names');
 
-        for (const org of orgs) {
-            const option = document.createElement("option");
-            option.text = org.name;
-            option.value = org.id;
-            select.add(option);
-        }
-        select.addEventListener('change', event => {
-            const idValue = parseInt(event.target.value);
-            if (!!idValue) {
-                Organization.displayOrgInformation(event);
-            } else {
-                document.querySelector("#main-msg").innerHTML = "<p>Information for organization's performances will appear here.</p>";
-            }
-        })
-    }
+//         for (const org of orgs) {
+//             const option = document.createElement("option");
+//             option.text = org.name;
+//             option.value = org.id;
+//             select.add(option);
+//         }
+//         select.addEventListener('change', event => {
+//             const idValue = parseInt(event.target.value);
+//             if (!!idValue) {
+//                 Organization.displayOrgInformation(event);
+//             } else {
+//                 document.querySelector("#main-msg").innerHTML = "<p>Information for organization's performances will appear here.</p>";
+//             }
+//         })
+//     }
 
-    static displayOrgInformation(event) {
-        const selected = event.target;
+//     static displayOrgInformation(event) {
+//         const selected = event.target;
 
-        fetch(`${ORGS_URL}/${selected.value}`)
-        .then(response => {
-            return response.json();
-        })
-        .then(object => {
-            console.log(object);
-            const div = document.querySelector("#main-msg");
-            let pDetail = document.createElement("p");
-            let pDescription = document.createElement("p");
+//         fetch(`${ORGS_URL}/${selected.value}`)
+//         .then(response => {
+//             return response.json();
+//         })
+//         .then(object => {
+//             console.log(object);
+//             const div = document.querySelector("#main-msg");
+//             let pDetail = document.createElement("p");
+//             let pDescription = document.createElement("p");
 
-            // clear placeholder
-            div.innerHTML = "";
+//             // clear placeholder
+//             div.innerHTML = "";
 
-            // add founding year and url
-            pDetail.innerHTML = `<strong>Founding year:</strong> ${object.year_founded}<br/><strong>URL:</strong> <a href="${object.url}" target="_blank">${object.url}</a>`;
-            div.appendChild(pDetail);
+//             // add founding year and url
+//             pDetail.innerHTML = `<strong>Founding year:</strong> ${object.year_founded}<br/><strong>URL:</strong> <a href="${object.url}" target="_blank">${object.url}</a>`;
+//             div.appendChild(pDetail);
 
-            // add description
-            pDescription.innerHTML = `<strong>Description:</strong> ${object.org_descr}`;
-            div.appendChild(pDescription);
-        })
-        .catch(message => {
-            console.log(message);
-        })
+//             // add description
+//             pDescription.innerHTML = `<strong>Description:</strong> ${object.org_descr}`;
+//             div.appendChild(pDescription);
+//         })
+//         .catch(message => {
+//             console.log(message);
+//         })
 
-    }
+//     }
 
-}
+// }
