0e89bd3c78aecfaf152b4d3ae06b51d3787dcf34
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-22 21:43:00 -0500

    Restored plain version of organization_serializer

--- backend/app/services/organization_serializer.rb
+++ backend/app/services/organization_serializer.rb
@@ -6,16 +6,6 @@ class OrganizationSerializer
 
     def to_serialized_json
         options =  {
-            include: {
-                performances: {
-                    only: [:performance_year],
-                    include: {
-                        composition: {
-                            only: [:name]
-                        }
-                    }
-                }
-            },
             except: [:updated_at, :created_at]
         }
         @org.to_json(options)
