92d809d78174d562d5c941deeee148d7904406f9
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-23 14:54:36 -0500

    Working on rendering compositions within performance class

--- frontend/src/performance.js
+++ frontend/src/performance.js
@@ -10,19 +10,27 @@ class Performance {
         return this._performanceYear;
     }
 
-    // compositions need to be rendered as composition objects?
+    getPerformanceCompositions() {
+        const comps = this._compositions.map(item => {
+            const comp = new Composition(item);
+            return comp;
+        });
+        console.log(comps);
+    }
 
     renderPerformanceData() {
         const div = document.createElement("div");
         const h3 = document.createElement("h3");
-        const pEdit = document.createElement("p");
+
+        this.getPerformanceCompositions();
+        // const comps = this.getPerformanceCompositions();
+        // console.log(typeof comps);
 
         h3.innerHTML = this.performanceYear;
         div.appendChild(h3);
 
-        pEdit.innerHTML = '<a href="#">Edit performance</a>';
-        div.appendChild(pEdit);
-        
+        div.innerHTML += '<p><a href="#">Edit performance</a></p>';
+
         div.classList.add("detail-container-item");
         return div;
     }
