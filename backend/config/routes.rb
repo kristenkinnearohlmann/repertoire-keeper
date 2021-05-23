e34d8e4859270f860fafbbfadfe34b39879c4871
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-22 21:15:19 -0500

    Clean up in routes.rb

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,12 +1,9 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  resources :performance_compositions
   root to: "static#home"
 
-  resources :organizations do
-    resources :performances, only: [:show]
-  end
-
+  resources :organizations
   resources :compositions
   resources :performances
+  resources :performance_compositions
 end
