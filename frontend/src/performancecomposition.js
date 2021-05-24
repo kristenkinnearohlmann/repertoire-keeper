8fd8cfb2db6ed131379c7dadd8b814243ee45a4c
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-23 21:33:42 -0500

    Filled in performance composition JS class

--- frontend/src/performancecomposition.js
+++ frontend/src/performancecomposition.js
@@ -1,3 +1,14 @@
 class PerformanceComposition {
-    
+    constructor(data) {
+        this._performanceId = data.performance_id;
+        this._compositionId = data.composition_id;
+    }
+
+    get performanceId() {
+        return this._performanceId;
+    }
+
+    get compositionId() {
+        return this._compositionId;
+    }
 }
