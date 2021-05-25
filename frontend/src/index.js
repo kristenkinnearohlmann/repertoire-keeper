956a8e1e88ef9586c1ea632cda8f5c7fa1875c7a
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-24 21:53:09 -0500

    Implemented addition of new composition, need to get proper version with adding the delete button

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -159,32 +159,29 @@ function addPerformanceComposition(event) {
 function addCompositionToPerformance(event) {
     event.preventDefault();
     const compForm = event.target;
-    console.log(compForm);
+    const perfCompList = document.getElementById(`perf-${compForm.performance_id.value}-comps`);
 
-    // const configObj = {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //         "Accept": "application/json"
-    //     },
-    //     body: JSON.stringify({
-    //         "name": compForm.comp_name.value,
-    //         "composer_lastname": compForm.comp_composer_lastname.value,
-    //         "composer_firstname": compForm.comp_composer_firstname.value,
-    //         "year_composed": compForm.comp_year_composed.value
-    //     })
-    // };
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": compForm.comp_name.value,
+            "composer_lastname": compForm.comp_composer_lastname.value,
+            "composer_firstname": compForm.comp_composer_firstname.value,
+            "year_composed": compForm.comp_year_composed.value
+        })
+    };
 
-    // fetch(`${BASE_URL}/performances/${compForm.performance_id.value}/performance_compositions`, configObj)
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         console.log(data);
-    //         console.log(data.composition);
-    //         const comp = new Composition(data.composition);
-    //         console.log(comp.renderCompositionListItem());
-    //         // console.log(comp);
-    //         // cancelEditPerformance();
-    //     })
+    fetch(`${BASE_URL}/performances/${compForm.performance_id.value}/performance_compositions`, configObj)
+        .then(response => response.json())
+        .then(data => {
+            const comp = new Composition(data.composition);
+            perfCompList.appendChild(comp.renderCompositionListItem());
+            cancelEditPerformance();
+        })
 };
 
 function cancelEditPerformance(event) {
